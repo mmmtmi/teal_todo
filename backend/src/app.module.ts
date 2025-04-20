e2048cc22f1b857bc275c469d7912f5d8a668533
mmmtmi <minamii.tks@gmail.com>
2025-04-20 12:40:19 +0900

    conected nest to DB

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Test } from './test.entity';
+
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'banana',
+      database: 'teal_todo',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'], // ← ここ
+      synchronize: true,
+      logging: true, // ← 追加でクエリ出力も確認できる
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
+
 })
 export class AppModule {}
