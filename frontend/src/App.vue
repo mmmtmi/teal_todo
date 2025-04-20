100a8a5ce4921528fad670d65d4797b9ed06aad6
mmmtmi <minamii.tks@gmail.com>
2025-04-20 21:35:42 +0900

    end Tutorial

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,63 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      title: 'My New Vue Title',
+      message: 'Welcome to Vue',
+      isRed: true,
+      input: {
+        firstName: '',
+        lastName: '',
+        isMember: true,
+      },
+      users: [
+        {
+          firstName: 'John',
+          lastName: 'Smith',
+          isMember: true,
+        },
+        {
+          firstName: 'Taro',
+          lastName: 'Shinjuku',
+          isMember: false,
+        },
+        {
+          firstName: 'Hanako',
+          lastName: 'Shibuya',
+          isMember: true,
+        },
+      ],
+    }
+  },
+  methods: {
+    addUser(){
+      this.users.push(this.input)
+      this.input = {
+        firstName: '',
+        lastName: '',
+        isMember: true,
+      }
+    },
+  },
 }
 </script>
 
+<template>
+  <h1 :title="message" :class="{ red: isRed }">{{ title }}</h1>
+  <input type="text" v-model="input.firstName"/>
+  <input type="text" v-model="input.lastName"/> 
+  <input type="checkbox" v-model="input.isMember"/>
+  <button @click="addUser">ユーザー追加</button>
+  <h2>ユーザーのデータ</h2>
+  <div v-for="(user,index) in users" :key="index">
+    <p>Name: {{ user.firstName + ' ' + user.lastName }}</p>
+    <p v-if="user.isMember">メンバーです</p>
+    <p v-else>メンバーではありません</p>
+  </div>
+</template>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  .red {
+    color: red;
+  }
+</style> 
