b51524e3552dbcec89bef9291719ef6f5e0dbfe2
mmmtmi <right1903@gmail.com>
2025-05-02 01:44:44 +0900

    fix .env

--- frontend/todo2/vite.config.ts
+++ frontend/todo2/vite.config.ts
@@ -1,23 +1,28 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import{ resolve } from 'path'
-import  * as path from 'path'
+import * as path from 'path'
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
 
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
 export default defineConfig({
-  
+
+
   server: {
     allowedHosts: ['molynote.com'],
   },
-
-  resolve:{
-    alias:{
-      '@': resolve(__dirname, 'src'),
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
       '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
     },
   },
   plugins: [vue()],
+
+  define: {
+    'process.env': {}
+  },
 })
-
-
