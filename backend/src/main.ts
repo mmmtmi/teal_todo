b88d4d2d43968e58cbf37488ed35d92a189ed090
mmmtmi <right1903@gmail.com>
2025-05-11 23:58:04 +0900

    log config

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,23 +1,40 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger, ValidationPipe } from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common';
 import { AllExceptionsFilter } from './fillter';
+import { WinstonModule } from 'nest-winston';
+import * as winston from 'winston';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    // ログを有効化
-    logger: ['log', 'warn', 'error', 'debug', 'verbose'], 
+  const logger = WinstonModule.createLogger({
+    transports: [
+      new winston.transports.Console({
+        format: winston.format.combine(
+          winston.format.colorize(),
+          winston.format.timestamp(),
+          winston.format.printf(({ timestamp, level, message }) => {
+            return `[${timestamp}] ${level}: ${message}`;
+          }),
+        ),
+      }),
+      new winston.transports.File({
+        filename: 'logs/app.log',
+        level: 'info',
+      }),
+    ],
   });
+
+  const app = await NestFactory.create(AppModule, {
+    logger,
+  });
+
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
-    origin: ['http://localhost:5173','http://localhost:8080']
+    origin: ['http://localhost:5173', 'http://localhost:8080'],
   });
-  app.useLogger(new Logger());
-  app.enableCors();
   app.useGlobalFilters(new AllExceptionsFilter());
- app.setGlobalPrefix('api');
+  app.setGlobalPrefix('api');
 
   await app.listen(process.env.PORT ?? 3000);
 }
-
 bootstrap();
